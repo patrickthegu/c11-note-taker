6dc3580e3e680463d3c110c337f726f8fa73cf4b
Patrick Gu <patrickthegu@gmail.com>
2022-08-08 16:35:40 +1000

    Change Port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 // Require JSON file and assign to noteData
 const noteData = require('./db/db.json');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Initialize app variable
 const app = express();
